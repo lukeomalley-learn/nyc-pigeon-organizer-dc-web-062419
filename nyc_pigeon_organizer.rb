ff3ca2530f08506ec1e2743bd513bba4cd20b068
Luke O'Malley <lukeomalley95@gmail.com>
2019-05-03 23:51:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,9 +8,22 @@ def nyc_pigeon_organizer(data)
             pigeon_list[name][trait.to_sym] << details.to_s
           else
             pigeon_list[name][trait.to_sym] = []
+            pigeon_list[name][trait.to_sym] << details.to_sym
           end
         else
           pigeon_list[name] = {}
+        end
+        
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+        
+        if !pigeon_list[name].has_key?(trait)
+          pigeon_list[name][trait.to_sym] = []
+        end
+        
+        if !pigeon_list[name][trait.to_sym].include?(details)
+          pigeon_list[name][trait.to_sym] << details.to_s
         end
       end
     end
